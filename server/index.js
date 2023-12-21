edc5316b51616663dcc468f301c3c47086480fa1
baris.tas <baris.tas@adesso.com.tr>
2023-12-21 17:53:33 +0300

    remove comment lines

--- server/index.js
+++ server/index.js
@@ -11,21 +11,6 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
-// app.use(express.json());
-
-// async function connectDb() {
-//   const uri = 'mongodb+srv://baris:6849358bt@cluster0.0noydrj.mongodb.net/merngraph_db?retryWrites=true&w=majority';
-//   try {
-//     await mongoose.connect(uri, {});
-//   } catch (error) {
-//     console.log(error);
-//   }
-// }
-// connectDb();
-
-// app.get('/', (req, res) => {
-//   res.send('Home Page');
-// });
 
 connectDB();
 
